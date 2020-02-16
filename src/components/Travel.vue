df09dee2539dbaab4659784985d3a6f0d374bfd4
lurkaround <rhelon@gmail.com>
2020-02-16 12:52:56 +0100

    initial commit

---
+++ src/components/Travel.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="hello">
+   {{priceArray}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Travel',
+  props: {
+    msg: String
+  },
+  computed: {
+    storeData(){
+      return this.$store.state.apiresult;
+    }
+  },
+  mounted(){
+    console.log(2)
+      //  let travelPrices = [];
+       Object.keys(this.storeData).forEach(function (item) {
+        console.log(item); // key
+        console.log(this.storeData[item]); // value
+        Object.keys(this.storeData[item]).forEach(function (item2){
+          console.log(item2.price)
+        })
+       })
+}
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .red {
+    color: red;
+  }
+</style>
--- src/store/index.js
+++ src/store/index.js
@@ -3,12 +3,29 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+import axios from 'axios';
+
 export default new Vuex.Store({
   state: {
+    apiresult: null,
   },
   mutations: {
+    setApi(state,payload){
+      state.apiresult = payload;
+    }
   },
   actions: {
+    loadAPI({commit}){
+      axios
+        .get(
+          "http://www.virail.com/virail/v7/search/en_us?from=c.3173435&to=c.3169070&lang=en_us&dt=2020-02-16&currency=USD&adult_passengers=1"
+        )
+        .then(res => {
+          let payload = res.data.transportStatus;
+          console.log(payload);
+          commit("setApi", payload);
+        });
+    }
   },
   modules: {
   }
